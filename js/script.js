0ebb929f6f89eb28728f152d62936d78e6c0b36a
Albert Frosztega <alb.fro@wp.pl>
2021-05-10 11:31:49 +0200

    refactoring JS

--- js/script.js
+++ js/script.js
@@ -1,21 +1,43 @@
-let sectionMe__button = document.querySelector(".sectionMe__button");
+{
 
-let jsPhotoLego = document.querySelector(".jsPhotoCptAmeryka");
+    const onChangePictureClick = () => {
+        jsPhotoLego.classList.toggle("sectionMe__hidden");
+        jsPhotoCptAmeryka.classList.toggle("sectionMe__hidden");
+        sectionMe__button.addEventListener("click", onChangePictureClick);
+    };
 
-let jsPhotoCptAmeryka = document.querySelector(".jsPhotoLego");
-
-sectionMe__button.addEventListener("click", () => {
-    jsPhotoLego.classList.toggle("sectionMe__hidden");
-    jsPhotoCptAmeryka.classList.toggle("sectionMe__hidden");
-});
+    const sectionMe__button = document.querySelector(".sectionMe__button");
+    const jsPhotoLego = document.querySelector(".jsPhotoCptAmeryka");
+    const jsPhotoCptAmeryka = document.querySelector(".jsPhotoLego");
+    onChangePictureClick(jsPhotoCptAmeryka, jsPhotoLego);
 
 
-let sectionKontakt__contactButtonNumber = document.querySelector(".sectionKontakt__contactButtonNumber");
 
-let sectionKontakt__phoneNumber = document.querySelector(".sectionKontakt__phoneNumber");
 
-sectionKontakt__contactButtonNumber.addEventListener("click", () =>{sectionKontakt__phoneNumber.classList.toggle("sectionMe__hidden");
-sectionKontakt__contactButtonNumber.innerText = sectionKontakt__phoneNumber.classList.contains("hidden")
-? "Kontakt"
-: "UsuN Kontakt";
-});
+    const showContactButton = () => {
+        const sectionKontakt__phoneNumber = document.querySelector(
+            ".sectionKontakt__phoneNumber"
+        );
+        sectionKontakt__phoneNumber.classList.toggle("sectionMe__hidden");
+        sectionKontakt__contactButtonNumber.innerText = sectionKontakt__phoneNumber.classList.contains(
+            "sectionMe__hidden"
+        )
+            ? "Kontakt"
+            : "UsuN Kontakt";
+    };
+    const init = () => {
+        let sectionKontakt__contactButtonNumber = document.querySelector(
+            ".sectionKontakt__contactButtonNumber");
+
+        sectionKontakt__contactButtonNumber.addEventListener("click", showContactButton);
+
+
+
+    };
+    init();
+
+
+
+
+
+}

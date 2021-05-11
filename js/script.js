1d49409ab60b3b687264bd99cae691491dbd3161
Albert Frosztega <alb.fro@wp.pl>
2021-05-11 15:28:46 +0200

    code fixes

--- js/script.js
+++ js/script.js
@@ -1,35 +1,34 @@
 {
 
-    const onChangePictureClick = () => {
-        jsPhotoLego.classList.toggle("sectionMe__hidden");
-        jsPhotoCptAmeryka.classList.toggle("sectionMe__hidden");
-        sectionMe__button.addEventListener("click", onChangePictureClick);
-    };
-
-    const sectionMe__button = document.querySelector(".sectionMe__button");
-    const jsPhotoLego = document.querySelector(".jsPhotoCptAmeryka");
-    const jsPhotoCptAmeryka = document.querySelector(".jsPhotoLego");
-    onChangePictureClick(jsPhotoCptAmeryka, jsPhotoLego);
-
-
-
-
     const showContactButton = () => {
-        const sectionKontakt__phoneNumber = document.querySelector(
-            ".sectionKontakt__phoneNumber"
+        const sectionContact__phoneNumber = document.querySelector(
+            ".sectionContact__phoneNumber"
         );
-        sectionKontakt__phoneNumber.classList.toggle("sectionMe__hidden");
-        sectionKontakt__contactButtonNumber.innerText = sectionKontakt__phoneNumber.classList.contains(
+        sectionContact__phoneNumber.classList.toggle("sectionMe__hidden");
+        sectionContact__contactButtonNumber.innerText = sectionContact__phoneNumber.classList.contains(
             "sectionMe__hidden"
         )
             ? "Kontakt"
             : "UsuN Kontakt";
+
     };
     const init = () => {
-        let sectionKontakt__contactButtonNumber = document.querySelector(
-            ".sectionKontakt__contactButtonNumber");
+        const onChangePictureClick = () => {
+            PhotoLego.classList.toggle("sectionMe__hidden");
+            PhotoCptAmeryka.classList.toggle("sectionMe__hidden");
+            sectionMe__button.addEventListener("click", onChangePictureClick);
+        };
 
-        sectionKontakt__contactButtonNumber.addEventListener("click", showContactButton);
+        const sectionMe__button = document.querySelector(".sectionMe__button");
+        const PhotoLego = document.querySelector(".jsPhotoCptAmeryka");
+        const PhotoCptAmeryka = document.querySelector(".jsPhotoLego");
+        onChangePictureClick(PhotoCptAmeryka, PhotoLego);
+
+
+        const sectionContact__contactButtonNumber = document.querySelector(
+            ".sectionContact__contactButtonNumber");
+
+        sectionContact__contactButtonNumber.addEventListener("click", showContactButton);
 
 
 
